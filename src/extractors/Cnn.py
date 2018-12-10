c52271d29e5c5d04cc8d575d781120570b3d0de1
filipmg <filip.m.guzy@gmail.com>
2018-12-10 18:11:34 +0100

    Stirling db & cnn update

--- src/extractors/Cnn.py
+++ src/extractors/Cnn.py
@@ -1,16 +1,14 @@
 from utils import convertListOfNdarraysToNdarray
 
-import numpy as np
-import keras
 from keras.models import Sequential
 from keras.layers import Conv2D, Activation, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
 from keras import optimizers
 from keras.utils import np_utils
 
+
 class Cnn:
     def __init__(self, xTrain, yTrain, numberOfClasses):
         self.__numClasses = numberOfClasses
-
 
     def fit(self, xTrain, yTrain):
         xTrainCnn = self.__reshapeX(xTrain)
@@ -38,11 +36,9 @@ class Cnn:
 
         return convertListOfNdarraysToNdarray(features)
 
-
     def fit_transform(self, x, y):
         self.fit(x,y)
         return self.transform(x)
-
 
     def __reshapeX(self, x):
         numOfChannels = 1
@@ -68,29 +64,27 @@ class Cnn:
 
         numOfChannels = 1
         imgSideLen = xTrainCnn.shape[1]
-        cnn.add(Conv2D(input_shape=(imgSideLen, imgSideLen, numOfChannels),\
-            filters = 24, kernel_size = (5,5), strides = (3)))
-        cnn.add(Activation('relu'))
-
-        cnn.add(Conv2D(filters = 64, kernel_size = (3,3), strides = (2)))
-
+        cnn.add(Conv2D(input_shape=(imgSideLen, imgSideLen, numOfChannels),
+                       filters=32, kernel_size=(7, 7), strides=(5, 5)))
         #https://medium.com/deeper-learning/glossary-of-deep-learning-batch-normalisation-8266dcd2fa82
         #TODO: observation: does not learn at all without this normalization, tbd why
         cnn.add(BatchNormalization())
         cnn.add(Activation('relu'))
         cnn.add(Dropout(0.5))
-        cnn.add(MaxPooling2D(pool_size = 3))
 
-        cnn.add(Conv2D(filters = 96, kernel_size = (3,3), strides = (1)))
+        cnn.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
+        cnn.add(Conv2D(filters=64, kernel_size=(3, 3), strides=(1, 1)))
         cnn.add(Activation('relu'))
         cnn.add(Dropout(0.5))
 
+        cnn.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+
         cnn.add(Flatten())
-        cnn.add(Dense(500, activation='relu'))
-        #cnn.add(Dense(10, activation='softmax'))
+        cnn.add(Dense(200, activation='relu'))
         cnn.add(Dense(self.__numClasses, activation='softmax'))
 
-        cnn.compile(loss='categorical_crossentropy', optimizer = optimizers.Adadelta(), metrics = ['accuracy'])
-        cnn.fit(xTrainCnn, yTrainCnn, batch_size = 200, epochs = 10)
+        cnn.compile(loss='categorical_crossentropy', optimizer=optimizers.Adadelta(), metrics=['accuracy'])
+        cnn.fit(xTrainCnn, yTrainCnn, batch_size=32, epochs=50)
 
         return cnn
